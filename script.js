0c855ce2f87c2dfbdd75d22d5306aae974704091
Lucas Melo <luucasmell@gmail.com>
2019-04-18 08:10:27 -0300

    Anotacoes aula de funcao e for

---
+++ script.js
@@ -0,0 +1,67 @@
+// ITERAR LISTA COM FOR
+// let filmes = ['Bastardos Inglorios', 'KillBill', 'Caes de Aluguel','Pulp Fiction', 'Os 8 odiados', 'O Hostel'];
+
+// FAROFA FOR OF
+// for(let item of filmes ){
+//   console.log(item);
+// }
+
+// FARINHA FOR IN
+// for(let indice in filmes){
+//   console.log(filmes[indice])
+// }
+
+// let indice;
+// for(indice=0; indice<filmes.length; indice++){
+//   console.log(filmes[indice]);
+// }
+
+
+// function nomeDaFuncao(argumentos){
+  // return qualquer coisa que envolva os argumentos
+// }
+
+function calcMedia(n1,n2,n3){
+  return ((n1+n2+n3)/3).toFixed(1)
+}
+
+calcMedia = (n1,n2,n3) => {
+  return ((n1+n2+n3)/3).toFixed(1)
+}
+
+let casa = {
+  cozinha: ["fogao", "microondas","agua"],
+  sala: ["sofa","televisao"]
+} 
+
+let filmes=[
+  /* 0 */{titulo:"Star Wars", classificacao: 14},
+  /* 1 */{titulo:"Chureki", classificacao: 0},
+  /* 2 */{titulo:"Ninja Assassino", classificacao: 18}
+]
+// COMO ACESSAR OBJETO NA LISTA
+filmes[0].classificacao
+
+/* QUAL É A SUA IDADE? 
+  condição se a idada for menor ou igual classificacao,
+  mostrar o filme disponivel
+
+  ** FERRAMENTAS **
+  variavel e prompt
+  for in
+  if else 
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
