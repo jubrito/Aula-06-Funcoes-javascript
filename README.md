5b37519019fc6caf2c7dd59454c1760d1c79a1d8
Juliana Witzke de Brito <jbrito@ef5.local>
2020-03-04 17:35:46 -0300

    Aula 6 - Funções JavaScript

---
+++ README.md
@@ -0,0 +1,10 @@
+# Curso Estação Hack - FRONT-END + BACK-END (MasterTech em parceria com o Facebook)
+
+## AULA 6 - FUNÇÕES JAVASCRIPT
+
+**Principais funcionalidades/conceitos aprendidos:**
+Functions, Arrays de objetos, Objetos com arrays, Iteração em arrays, FOR OF, FOR IN, FOR padrão, Acesso de objetos em uma lista
+
+**Conceitos Complementares:**
+toFixed()
+
--- script.js
+++ script.js
@@ -1,49 +1,64 @@
 // ITERAR LISTA COM FOR
-// let filmes = ['Bastardos Inglorios', 'KillBill', 'Caes de Aluguel','Pulp Fiction', 'Os 8 odiados', 'O Hostel'];
+let array = ['Bastardos Inglorios', 'KillBill', 'Caes de Aluguel','Pulp Fiction', 'Os 8 odiados', 'O Hostel'];
+ 
+// FOR OF
+console.log("---------- Exibindo itens FOR OF ----------");
+for(let item of array){
+  console.log(item);
+}
 
-// FAROFA FOR OF
-// for(let item of filmes ){
-//   console.log(item);
-// }
+// FOR IN
+console.log("---------- Exibindo itens FOR IN ----------");
+for(let indice in array){
+  console.log(array[indice])
+}
 
-// FARINHA FOR IN
-// for(let indice in filmes){
-//   console.log(filmes[indice])
-// }
-
-// let indice;
-// for(indice=0; indice<filmes.length; indice++){
-//   console.log(filmes[indice]);
-// }
+let indice;
+console.log("---------- Exibindo itens FOR PADRÃO ----------");
+for(indice=0; indice<array.length; indice++){
+  console.log(array[indice]);
+}
 
 
 // function nomeDaFuncao(argumentos){
-  // return qualquer coisa que envolva os argumentos
+//   return qualquer coisa que envolva os argumentos
 // }
 
+/* O método toFixed() formata um número utilizando notação de ponto fixo.
+  
+  numObj.toFixed([dígitos])
+
+  Digitos (opcional): O número de dígitos que aparecem depois do ponto decimal; valor entre 0 e 20 (se omitido, valor = 0). 
+*/
+
+// FUNCTION
 function calcMedia(n1,n2,n3){
   return ((n1+n2+n3)/3).toFixed(1)
 }
 
+// ARROW FUNCTION
 calcMedia = (n1,n2,n3) => {
   return ((n1+n2+n3)/3).toFixed(1)
 }
 
+// OBJETO COM ARRAYS
 let casa = {
   cozinha: ["fogao", "microondas","agua"],
   sala: ["sofa","televisao"]
 } 
 
+// ARRAY DE OBJETOS
 let filmes=[
   /* 0 */{titulo:"Star Wars", classificacao: 14},
   /* 1 */{titulo:"Chureki", classificacao: 0},
   /* 2 */{titulo:"Ninja Assassino", classificacao: 18}
 ]
+
 // COMO ACESSAR OBJETO NA LISTA
 filmes[0].classificacao
 
 /* QUAL É A SUA IDADE? 
-  condição se a idada for menor ou igual classificacao,
+  condição se a idade for menor ou igual classificacao,
   mostrar o filme disponivel
 
   ** FERRAMENTAS **
